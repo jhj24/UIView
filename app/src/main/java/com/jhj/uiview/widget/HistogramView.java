4d72c2e9716871aa1c04ddc888a6f035d192ef7e
jhj24 <1406929846@qq.com>
2018-12-28 18:12:31 +0800

    commit

--- app/src/main/java/com/jhj/uiview/widget/HistogramView.java
+++ app/src/main/java/com/jhj/uiview/widget/HistogramView.java
@@ -7,7 +7,6 @@ import android.graphics.Paint;
 import android.graphics.Path;
 import android.graphics.Rect;
 import android.support.annotation.Nullable;
-import android.text.TextPaint;
 import android.util.AttributeSet;
 import android.view.View;
 
@@ -44,10 +43,10 @@ public class HistogramView extends View {
 
 
     private Paint mPaint;
-    private Paint mTextPaint;
     private Path mPath;
-    private List<HistogramBean> dataList;
-    private int maxTextHeight;
+    private List<HistogramBean> mDataList;
+    private int mMaxTextHeight;
+    private float num;
 
 
     public HistogramView(Context context) {
@@ -62,7 +61,11 @@ public class HistogramView extends View {
         super(context, attrs, defStyleAttr);
 
         mPaint = new Paint();
-        mTextPaint = new TextPaint();
+        mPaint.setColor(mVariableGroupColor);
+        mPaint.setTextSize(mVariableGroupSize);
+        mPaint.setStrokeWidth(4);
+
+
         mPath = new Path();
         float scaleDensity = context.getResources().getDisplayMetrics().scaledDensity;
         float density = context.getResources().getDisplayMetrics().density;
@@ -86,46 +89,27 @@ public class HistogramView extends View {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
-        //写描述以及计算描述的最大高度
-        for (int i = 0; i < dataList.size(); i++) {
-            HistogramBean bean = dataList.get(i);
-            int left = (int) (mHistogramDistance * (1 + i) + mHistogramWidth * i + getPaddingLeft());
 
-            Rect textRect = new Rect();
-            String text = bean.getName();
-            mTextPaint.reset();
-            mTextPaint.setColor(mVariableGroupColor);
-            mTextPaint.setTextSize(mVariableGroupSize);
-            mTextPaint.setStrokeWidth(4);
-            mTextPaint.getTextBounds(text, 0, text.length(), textRect);
-            if (textRect.height() > maxTextHeight) {
-                maxTextHeight = textRect.height();
-            }
-            int offset1 = (int) ((mHistogramWidth - textRect.width()) / 2);
-            canvas.drawText(text, left + offset1, getHeight() - getPaddingBottom(), mTextPaint);
-        }
-
-        //坐标图
-        mPaint.reset();
-        mPaint.setColor(mAxisLineColor);
-        mPaint.setStrokeWidth(mAxisLineWidth);
-        mPaint.setStyle(Paint.Style.STROKE);
-        mPath.moveTo(getPaddingLeft() + mAxisLineWidth / 2, getPaddingTop() + mAxisLineWidth / 2);
-        mPath.lineTo(getPaddingLeft() + mAxisLineWidth / 2, getHeight() - (mAxisLineWidth / 2 + getPaddingBottom() + maxTextHeight + mFrequencyMarginBottom));
-        mPath.lineTo(getWidth() - getPaddingRight() - mAxisLineWidth / 2, getHeight() - (mAxisLineWidth / 2 + getPaddingBottom() + maxTextHeight + mFrequencyMarginBottom));
-        canvas.drawPath(mPath, mPaint);
+        for (int i = 0; i < mDataList.size(); i++) {
+            HistogramBean bean = mDataList.get(i);
+            float height = getHeight() - (getPaddingBottom() + getPaddingTop() + mMaxTextHeight + mVariableGroupMarginTop + mAxisLineWidth);
+            float left = getPaddingLeft() + mAxisLineWidth + mHistogramDistance * (i + 1) + mHistogramWidth * i;
+            float top = height * (1 - bean.getPercent() / num);
+            float right = getPaddingLeft() + mAxisLineWidth + (mHistogramDistance + mHistogramWidth) * (i + 1);
+            float bottom = getHeight() - (getPaddingBottom() + mMaxTextHeight + mVariableGroupMarginTop + mAxisLineWidth);
 
 
-        for (int i = 0; i < dataList.size(); i++) {
-            HistogramBean bean = dataList.get(i);
+            //写描述
+            Rect bound = new Rect();
+            mPaint.reset();
+            mPaint.setColor(mVariableGroupColor);
+            mPaint.setTextSize(mVariableGroupSize);
+            mPaint.setStrokeWidth(4);
+            mPaint.getTextBounds(bean.getName(), 0, bean.getName().length(), bound);
+            float offset1 = (mHistogramWidth - bound.width()) / 2;
+            canvas.drawText(bean.getName(), left + offset1, getHeight() - getPaddingBottom(), mPaint);
 
             //画柱状图
-            int height = (int) (getHeight() - (getPaddingBottom() + getPaddingTop() + maxTextHeight + mFrequencyMarginBottom + mAxisLineWidth));
-            int left = (int) (mHistogramDistance * (1 + i) + mHistogramWidth * i + getPaddingLeft() + mAxisLineWidth);
-            int top = (int) (height * (1 - bean.getPercent()));
-            int right = (int) ((mHistogramDistance + mHistogramWidth) * (1 + i) + getPaddingLeft() + mAxisLineWidth);
-            int bottom = (int) (getHeight() - (getPaddingBottom() + maxTextHeight + mFrequencyMarginBottom + mAxisLineWidth));
-
             mPaint.reset();
             mPaint.setColor(mHistogramColor);
             mPaint.setStyle(Paint.Style.FILL);
@@ -140,16 +124,36 @@ public class HistogramView extends View {
             mPaint.setStrokeWidth(4);
             mPaint.getTextBounds(percent, 0, percent.length(), rect);
 
-            int offset = (int) ((mHistogramWidth - rect.width()) / 2);
-            int x = left + offset;
-            int y = (int) (top - mFrequencyMarginBottom);
+            float offset = (mHistogramWidth - rect.width()) / 2;
+            float x = left + offset;
+            float y = (int) (top - mFrequencyMarginBottom);
             canvas.drawText(percent, x, y, mPaint);
+
         }
+
+        //坐标图
+        float axisBottom = getHeight() - (getPaddingBottom() + mAxisLineWidth / 2 + mMaxTextHeight + mVariableGroupMarginTop);
+        mPaint.reset();
+        mPaint.setColor(mAxisLineColor);
+        mPaint.setStrokeWidth(mAxisLineWidth);
+        mPaint.setStyle(Paint.Style.STROKE);
+        mPath.moveTo(getPaddingLeft() + mAxisLineWidth / 2, getPaddingTop() + mAxisLineWidth / 2);
+        mPath.lineTo(getPaddingLeft() + mAxisLineWidth / 2, axisBottom);
+        mPath.lineTo(getWidth() - (getPaddingRight() + mAxisLineWidth / 2), axisBottom);
+        canvas.drawPath(mPath, mPaint);
     }
 
 
     public void setData(List<HistogramBean> list) {
-        this.dataList = list;
+        this.mDataList = list;
+        for (int i = 0; i < mDataList.size(); i++) {
+            Rect rect = new Rect();
+            mPaint.getTextBounds(mDataList.get(i).getName(), 0, mDataList.get(i).getName().length(), rect);
+            if (mMaxTextHeight < rect.height()) {
+                mMaxTextHeight = rect.height();
+            }
+            num += mDataList.get(i).getPercent();
+        }
     }
 
 }
--- app/src/main/java/com/jhj/uiview/widget/LineChartView.java
+++ app/src/main/java/com/jhj/uiview/widget/LineChartView.java
@@ -56,6 +56,7 @@ public class LineChartView extends View {
         TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.LineChartView);
         mLineChartColor = typedArray.getColor(R.styleable.LineChartView_lineChartColor, 0xff68b831);
         mLineChartDistance = typedArray.getDimension(R.styleable.LineChartView_lineChartDistance, 50 * density);
+        mLineChartWidth = typedArray.getInt(R.styleable.LineChartView_lineChartWidth, 3);
         mAxisLineColor = typedArray.getColor(R.styleable.LineChartView_axisLineColor, 0xFF68b831);
         mAxisLineWidth = typedArray.getInt(R.styleable.LineChartView_axisLineWidth, 3);
         mFrequencyColor = typedArray.getColor(R.styleable.LineChartView_frequencyColor, 0xff68b831);
@@ -78,16 +79,6 @@ public class LineChartView extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        //坐标轴
-        float axisBottom = getHeight() - (getPaddingBottom() + mAxisLineWidth / 2 + mMaxTextHeight + mVariableGroupMarginTop);
-        mPaint.setColor(mAxisLineColor);
-        mPaint.setStrokeWidth(mAxisLineWidth);
-        mPaint.setStyle(Paint.Style.STROKE);
-        mPath.moveTo(getPaddingLeft() + mAxisLineWidth / 2, getPaddingTop() + mAxisLineWidth / 2);
-        mPath.lineTo(getPaddingLeft() + mAxisLineWidth / 2, axisBottom);
-        mPath.lineTo(getWidth() - (getPaddingRight() + mAxisLineWidth / 2), axisBottom);
-        canvas.drawPath(mPath, mPaint);
-
 
         for (int i = 0; i < mDataList.size(); i++) {
             String text = mDataList.get(i).getName();
@@ -97,7 +88,7 @@ public class LineChartView extends View {
             //变量分组
             Rect bound = new Rect();
             mTextPaint.getTextBounds(text, 0, text.length(), bound);
-            float startX = x - bound.width();
+            float startX = x - bound.width() / 2;
             canvas.drawText(text, startX, getHeight() - getPaddingBottom(), mTextPaint);
 
             //画点
@@ -108,8 +99,6 @@ public class LineChartView extends View {
             float y = axisHeight * (1 - (mDataList.get(i).getPercent() / num));
             canvas.drawPoint(x, y, mPaint);
 
-            //折线图
-
             //比例
             String percent = String.valueOf(mDataList.get(i).getPercent());
             Rect rect = new Rect();
@@ -118,10 +107,33 @@ public class LineChartView extends View {
             mPaint.setTextSize(mFrequencySize);
             mPaint.setStrokeWidth(4);
             mPaint.getTextBounds(percent, 0, percent.length(), rect);
-            float offset = x - rect.width();
-            canvas.drawText(String.valueOf(mDataList.get(i).getPercent()), offset, y - mFrequencyMarginBottom, mPaint);
+            float offset = x - rect.width() / 2;
+            canvas.drawText(percent, offset, y - mFrequencyMarginBottom, mPaint);
 
+            //折线图
+            if (i == 0) {
+                mPath.moveTo(x, y);
+            }
+            mPath.lineTo(x, y);
+            if (i == mDataList.size() -1){
+                mPaint.reset();
+                mPaint.setColor(mLineChartColor);
+                mPaint.setStrokeWidth(mLineChartWidth);
+                mPaint.setStyle(Paint.Style.STROKE);
+                canvas.drawPath(mPath, mPaint);
+            }
         }
+
+        //坐标轴
+        float axisBottom = getHeight() - (getPaddingBottom() + mAxisLineWidth/2 + mMaxTextHeight + mVariableGroupMarginTop);
+        mPaint.reset();
+        mPaint.setColor(mAxisLineColor);
+        mPaint.setStrokeWidth(mAxisLineWidth);
+        mPaint.setStyle(Paint.Style.STROKE);
+        mPath.moveTo(getPaddingLeft() + mAxisLineWidth / 2, getPaddingTop() + mAxisLineWidth / 2);
+        mPath.lineTo(getPaddingLeft() + mAxisLineWidth / 2, axisBottom);
+        mPath.lineTo(getWidth() - (getPaddingRight() + mAxisLineWidth / 2), axisBottom);
+        canvas.drawPath(mPath, mPaint);
     }
 
 
